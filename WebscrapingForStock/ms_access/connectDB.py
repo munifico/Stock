3e9683cb018c01a4fd208e0243ba119da616ff2e
sideok <sid0508@naver.com>
2019-07-13 05:10:37 +0900

    DB경로 변경

--- WebscrapingForStock/ms_access/connectDB.py
+++ WebscrapingForStock/ms_access/connectDB.py
@@ -7,13 +7,13 @@ import pyodbc;
 
 class StockDB :
     def __init__(self) :
-        self.conn = pyodbc.connect(Driver='{Microsoft Access Driver (*.mdb, *.accdb)}',DBQ='C:\\Users\\SIDeok\\git\\WebscrapingForStock\\WebscrapingForStock\\ms_access\\StockDB.mdb')
+        self.conn = pyodbc.connect(Driver='{Microsoft Access Driver (*.mdb, *.accdb)}',DBQ='D:\주식 백테스트\workspace\WebscrapingForStock\ms_access\StockDB.mdb')
         
     def getCursor(self) :
         try :
             return self.conn.cursor()
         except AttributeError :
-            self.conn = pyodbc.connect(Driver='{Microsoft Access Driver (*.mdb, *.accdb)}',DBQ='C:\\Users\\SIDeok\\git\\WebscrapingForStock\\WebscrapingForStock\\ms_access\\StockDB.mdb')
+            self.conn = pyodbc.connect(Driver='{Microsoft Access Driver (*.mdb, *.accdb)}',DBQ='D:\주식 백테스트\workspace\WebscrapingForStock\ms_access\StockDB.mdb')
             return self.conn.cursor()
     
     def setRelease(self) :
