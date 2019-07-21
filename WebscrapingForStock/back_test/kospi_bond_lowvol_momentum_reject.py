708b2b67012a9d6aeb0bd9b536474d59890c5a79
sideok <sid0508@naver.com>
2019-07-22 00:38:40 +0900

    과최적화 소스 이름변경

--- WebscrapingForStock/back_test/kospi_bond_lowvol_momentum_reject.py
+++ WebscrapingForStock/back_test/kospi_bond_lowvol_momentum_reject.py
@@ -5,6 +5,8 @@ Created on 2019. 7. 13.
 수익측정 백테스트
 
 일간 손절매 로직 추가 필요.
+
+과최적화로 인한 폐기소스
 @author: SIDeok
 '''
 from cmath import sqrt
@@ -45,7 +47,7 @@ BASE_DT = []
 VALUE = [];
 
 i = -1;
-value = 4000; #초기 투입 금액
+value = 1; #초기 투입 금액
 
 # 연복리 산출을 위한 변수
 start_val = value;
@@ -57,32 +59,20 @@ mdd = 0; # mdd
 mdd_dt = ""; # mdd 대상일
 for info in rows :
     i += 1;
-    if i > 0 :
-        info = [info[0], info[1], info[2], rowsList[i-1][3]*info[3]
-                                         , rowsList[i-1][4]*info[4]
-                                         , rowsList[i-1][5]*info[5]
-                                         , rowsList[i-1][6]*info[6]
-                                         , rowsList[i-1][7]*info[7]
-                                         , rowsList[i-1][8]*info[8]
-                                         , rowsList[i-1][9]*info[9]
-                                         , rowsList[i-1][10]*info[10]
-                                         , rowsList[i-1][11]*info[11]
-                                         , rowsList[i-1][12]*info[12]
-                                         , rowsList[i-1][13]*info[13] ];
     rowsList.append(info);
-    if i < 6 : continue;
+    if i < 5 : continue;
     
-    V10_0 = ((rowsList[i-1][3 ] - rowsList[i-6][3 ])/rowsList[i-6][3 ])/numpy.std([rowsList[i-6][3 ], rowsList[i-5][3 ], rowsList[i-4][3 ], rowsList[i-3][3 ], rowsList[i-2][3 ], rowsList[i-1][3 ]]);
-    V9_1  = ((rowsList[i-1][4 ] - rowsList[i-6][4 ])/rowsList[i-6][4 ])/numpy.std([rowsList[i-6][4 ], rowsList[i-5][4 ], rowsList[i-4][4 ], rowsList[i-3][4 ], rowsList[i-2][4 ], rowsList[i-1][4 ]]);
-    V8_2  = ((rowsList[i-1][5 ] - rowsList[i-6][5 ])/rowsList[i-6][5 ])/numpy.std([rowsList[i-6][5 ], rowsList[i-5][5 ], rowsList[i-4][5 ], rowsList[i-3][5 ], rowsList[i-2][5 ], rowsList[i-1][5 ]]);
-    V7_3  = ((rowsList[i-1][6 ] - rowsList[i-6][6 ])/rowsList[i-6][6 ])/numpy.std([rowsList[i-6][6 ], rowsList[i-5][6 ], rowsList[i-4][6 ], rowsList[i-3][6 ], rowsList[i-2][6 ], rowsList[i-1][6 ]]);
-    V6_4  = ((rowsList[i-1][7 ] - rowsList[i-6][7 ])/rowsList[i-6][7 ])/numpy.std([rowsList[i-6][7 ], rowsList[i-5][7 ], rowsList[i-4][7 ], rowsList[i-3][7 ], rowsList[i-2][7 ], rowsList[i-1][7 ]]);
-    V5_5  = ((rowsList[i-1][8 ] - rowsList[i-6][8 ])/rowsList[i-6][8 ])/numpy.std([rowsList[i-6][8 ], rowsList[i-5][8 ], rowsList[i-4][8 ], rowsList[i-3][8 ], rowsList[i-2][8 ], rowsList[i-1][8 ]]);
-    V4_6  = ((rowsList[i-1][9 ] - rowsList[i-6][9 ])/rowsList[i-6][9 ])/numpy.std([rowsList[i-6][9 ], rowsList[i-5][9 ], rowsList[i-4][9 ], rowsList[i-3][9 ], rowsList[i-2][9 ], rowsList[i-1][9 ]]);
-    V3_7  = ((rowsList[i-1][10] - rowsList[i-6][10])/rowsList[i-6][10])/numpy.std([rowsList[i-6][10], rowsList[i-5][10], rowsList[i-4][10], rowsList[i-3][10], rowsList[i-2][10], rowsList[i-1][10]]);
-    V2_8  = ((rowsList[i-1][11] - rowsList[i-6][11])/rowsList[i-6][11])/numpy.std([rowsList[i-6][11], rowsList[i-5][11], rowsList[i-4][11], rowsList[i-3][11], rowsList[i-2][11], rowsList[i-1][11]]);
-    V1_9  = ((rowsList[i-1][12] - rowsList[i-6][12])/rowsList[i-6][12])/numpy.std([rowsList[i-6][12], rowsList[i-5][12], rowsList[i-4][12], rowsList[i-3][12], rowsList[i-2][12], rowsList[i-1][12]]);
-    V0_10 = ((rowsList[i-1][13] - rowsList[i-6][13])/rowsList[i-6][13])/numpy.std([rowsList[i-6][13], rowsList[i-5][13], rowsList[i-4][13], rowsList[i-3][13], rowsList[i-2][13], rowsList[i-1][13]]);
+    V10_0 = ((rowsList[i][3 ] - rowsList[i-5][3 ])/rowsList[i-5][3 ])/numpy.std([rowsList[i-5][3 ], rowsList[i-4][3 ], rowsList[i-3][3 ], rowsList[i-2][3 ], rowsList[i-1][3 ], rowsList[i][3 ]]);
+    V9_1  = ((rowsList[i][4 ] - rowsList[i-5][4 ])/rowsList[i-5][4 ])/numpy.std([rowsList[i-5][4 ], rowsList[i-4][4 ], rowsList[i-3][4 ], rowsList[i-2][4 ], rowsList[i-1][4 ], rowsList[i][4 ]]);
+    V8_2  = ((rowsList[i][5 ] - rowsList[i-5][5 ])/rowsList[i-5][5 ])/numpy.std([rowsList[i-5][5 ], rowsList[i-4][5 ], rowsList[i-3][5 ], rowsList[i-2][5 ], rowsList[i-1][5 ], rowsList[i][5 ]]);
+    V7_3  = ((rowsList[i][6 ] - rowsList[i-5][6 ])/rowsList[i-5][6 ])/numpy.std([rowsList[i-5][6 ], rowsList[i-4][6 ], rowsList[i-3][6 ], rowsList[i-2][6 ], rowsList[i-1][6 ], rowsList[i][6 ]]);
+    V6_4  = ((rowsList[i][7 ] - rowsList[i-5][7 ])/rowsList[i-5][7 ])/numpy.std([rowsList[i-5][7 ], rowsList[i-4][7 ], rowsList[i-3][7 ], rowsList[i-2][7 ], rowsList[i-1][7 ], rowsList[i][7 ]]);
+    V5_5  = ((rowsList[i][8 ] - rowsList[i-5][8 ])/rowsList[i-5][8 ])/numpy.std([rowsList[i-5][8 ], rowsList[i-4][8 ], rowsList[i-3][8 ], rowsList[i-2][8 ], rowsList[i-1][8 ], rowsList[i][8 ]]);
+    V4_6  = ((rowsList[i][9 ] - rowsList[i-5][9 ])/rowsList[i-5][9 ])/numpy.std([rowsList[i-5][9 ], rowsList[i-4][9 ], rowsList[i-3][9 ], rowsList[i-2][9 ], rowsList[i-1][9 ], rowsList[i][9 ]]);
+    V3_7  = ((rowsList[i][10] - rowsList[i-5][10])/rowsList[i-5][10])/numpy.std([rowsList[i-5][10], rowsList[i-4][10], rowsList[i-3][10], rowsList[i-2][10], rowsList[i-1][10], rowsList[i][10]]);
+    V2_8  = ((rowsList[i][11] - rowsList[i-5][11])/rowsList[i-5][11])/numpy.std([rowsList[i-5][11], rowsList[i-4][11], rowsList[i-3][11], rowsList[i-2][11], rowsList[i-1][11], rowsList[i][11]]);
+    V1_9  = ((rowsList[i][12] - rowsList[i-5][12])/rowsList[i-5][12])/numpy.std([rowsList[i-5][12], rowsList[i-4][12], rowsList[i-3][12], rowsList[i-2][12], rowsList[i-1][12], rowsList[i][12]]);
+    V0_10 = ((rowsList[i][13] - rowsList[i-5][13])/rowsList[i-5][13])/numpy.std([rowsList[i-5][13], rowsList[i-4][13], rowsList[i-3][13], rowsList[i-2][13], rowsList[i-1][13], rowsList[i][13]]);
     
     tmpDic = {3:V10_0, 4:V9_1, 5:V8_2, 6:V7_3, 7:V6_4, 8:V5_5, 9:V4_6, 10:V3_7, 11:V2_8, 12:V1_9, 13:V0_10};
     
@@ -95,25 +85,24 @@ for info in rows :
         for data in tmpDic :
             if tmpIdx == -1 or tmpDic[data] > tmpDic[tmpIdx] :
                 tmpIdx = data;
-                
         if tmpDic[tmpIdx] < 0 :
-            sum = value;
+            sum = 1;
             choice += ", -1";
             if tmp_rg == 0 :
                 tmp_rg = 1;
             break;
         else :
-            sum += value * (1 + (rowsList[i][tmpIdx] - rowsList[i-1][tmpIdx])/rowsList[i-1][tmpIdx]);
             tmpDic.pop(tmpIdx);
+            sum += rowsList[i][tmpIdx];
             choice += ", " + str(tmpIdx);
             tmp_rg += 1;
     
 #     print(str(info[0]) + "    " + str(sum*value/tmp_rg+60) + "    " + choice);
-    print(str(info[0]) + "    " + str(sum/tmp_rg) + "    " + choice);
+    print(str(info[0]) + "    " + str(sum*value/tmp_rg) + "    " + choice);
     BASE_DT.append(info[0]);
-    VALUE.append(sum/tmp_rg);
+    VALUE.append(sum*value/tmp_rg);
 #     value = sum*value/tmp_rg + 60;
-    value = sum/tmp_rg;
+    value = sum*value/tmp_rg;
     
     # mdd 계산
     if value >= max_val :
